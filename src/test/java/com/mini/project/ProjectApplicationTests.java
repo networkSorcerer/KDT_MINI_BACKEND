5988f86910802b58e88ad0ded6501eec5695a296
suddengithub <konyu1007@gmail.com>
2024-11-29 17:36:49 +0900

    Update ProjectApplicationTests.java

--- src/test/java/com/mini/project/ProjectApplicationTests.java
+++ src/test/java/com/mini/project/ProjectApplicationTests.java
@@ -5,11 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ProjectApplicationTests {
-	@Test
-	void contextLoads() {
-	}
-
-	
+ 
 	@Test
 	void contextLoads() {
 	}
